d920087e686ec82070f6933990452a52a1a87403
aditya <aditya@aditya>
2013-12-18 14:48:29 +0530

    Added random array generation and functionality to check whether array is sorted or not..

--- src/researchpaper/ResearchPaper.java
+++ src/researchpaper/ResearchPaper.java
@@ -14,11 +14,30 @@ public class ResearchPaper {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException {
-        int a[] = {1, 12, 2, 11, 3, 10, 4, 9, 5, 8, 6, 7};
+        ResearchPaper rpobj1 = new ResearchPaper();
+        int size = 10000000;
+        int a[] = new int[size];
+        rpobj1.fillRandom(a, size);
         int numOfChunks = 4;
         ParallelSort psort1 = new ParallelSort(a, numOfChunks);
-        psort1.printArray();
+        //psort1.printArray();
         psort1.sort();
-        psort1.printArray();
+        System.out.println(rpobj1.isSorted(a));
+        //psort1.printArray();
+    }
+
+    public void fillRandom(int[] arr, int range) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * range);
+        }
+    }
+
+    public boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
