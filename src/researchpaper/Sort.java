c12c3cad406f52a95cee5b77a6e02f5483396c28
aditya <aditya@aditya>
2014-01-16 08:49:58 +0530

    Added Both sort to sort class

--- src/researchpaper/Sort.java
+++ src/researchpaper/Sort.java
@@ -61,3 +61,37 @@ public class Sort {
         }
     }
 }
+
+/*public class Sort {
+
+    public void sort(int[] a, int start, int end) {
+        if (start < end) {
+            int key = partitionArray(a, start, end);
+            quickSort(a, start, key - 1);
+            quickSort(a, key + 1, end);
+        }
+    }
+
+    public int partitionArray(int[] arr, int start, int end) {
+        int pivot = start + (int) (Math.random() * (end - start + 1));
+        swapElement(arr, start, pivot);
+        int key = arr[start];
+        int i = start;
+        for (int j = i + 1; j <= end; j++) {
+            if (arr[j] < key) {
+                i++;
+                swapElement(arr, i, j);
+            }
+
+        }
+        swapElement(arr, start, i);
+        return i;
+    }
+
+    public void swapElement(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
+*/
